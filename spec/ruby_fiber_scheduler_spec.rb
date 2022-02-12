7dc3adba62f8892330ea9f3105215a739e1cd027
Bruno Sutic <code@brunosutic.com>
2022-02-12 20:47:47 +0100

    Correct ruby's fiber scheduler spec

--- spec/ruby_fiber_scheduler_spec.rb
+++ spec/ruby_fiber_scheduler_spec.rb
@@ -1,5 +1,12 @@
 RSpec.describe "Ruby fiber scheduler" do
   let(:scheduler_class) { IOBufferScheduler }
 
-  include_examples FiberSchedulerSpec
+  include_examples FiberSchedulerSpec::BlockUnblock
+  include_examples FiberSchedulerSpec::Close
+  include_examples FiberSchedulerSpec::Fiber
+  # include_examples FiberSchedulerSpec::IOWait
+  include_examples FiberSchedulerSpec::KernelSleep
+  include_examples FiberSchedulerSpec::ProcessWait
+  include_examples FiberSchedulerSpec::SocketIO
+  include_examples FiberSchedulerSpec::TimeoutAfter
 end
