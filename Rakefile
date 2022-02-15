3c74ae71a4e8a9f2726337c9bc60fa334a663543
Bruno Sutic <code@brunosutic.com>
2022-02-15 11:55:00 +0100

    Test io-event scheduler

--- Rakefile
+++ Rakefile
@@ -9,6 +9,15 @@ namespace :fetch do
       mode: "w"
     )
   end
+
+  task :io_event do
+    url = "https://raw.githubusercontent.com/socketry/io-event/main/examples/scheduler/scheduler.rb"
+    File.write(
+      "spec/support/fiber_scheduler/io_event.rb",
+      URI.parse(url).open.read,
+      mode: "w"
+    )
+  end
 end
 
-task fetch: %i[fetch:ruby]
+task fetch: %i[fetch:ruby fetch:io_event]
---
+++ spec/io_event_fiber_scheduler_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe IO::Event::Scheduler do
+  # include_examples FiberSchedulerSpec::BlockUnblock
+  include_examples FiberSchedulerSpec::Close
+  include_examples FiberSchedulerSpec::Fiber
+  # include_examples FiberSchedulerSpec::IOWait
+  include_examples FiberSchedulerSpec::KernelSleep
+  # include_examples FiberSchedulerSpec::NestedFiberSchedule
+  # include_examples FiberSchedulerSpec::ProcessWait
+  # include_examples FiberSchedulerSpec::SocketIO
+  include_examples FiberSchedulerSpec::TimeoutAfter
+end
